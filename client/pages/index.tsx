9c4dcd989e4788627ece3d8b5183a2a5e5e2b1de
Funmilola O <funmilolajire@gmail.com>
2021-05-07 15:27:42 +0100

    api endpoints

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -29,7 +29,7 @@ const Home: FC<Invoices> = ({ invoices }) => {
 
   useEffect(() => {
     getInvoices()
-  }, [status])
+  })
 
   return (
     <>
--- server/src/invoices/invoices.service.ts
+++ server/src/invoices/invoices.service.ts
@@ -15,7 +15,7 @@ export class InvoicesService {
   }
 
   async findAll() {
-    return await this.invoiceModel.find({}).exec()
+    return await this.invoiceModel.find({}).sort({ createdAt: -1 }).exec()
   }
 
   async findByTerm(term: string) {
